e32a546699b72e94a1380cf5727c3153b886c77c
Ondřej Bárta <email@ondrejbarta.xyz>
2025-04-24 10:11:34 +0200

    feat: create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "GCP region"
+  default     = "europe-west1"
+}
+
+variable "storage_bucket_name" {
+  description = "Name of the storage bucket"
+  default     = "life-sync-bucket"
+}
+
+variable "function_name" {
+  description = "Name of the Cloud Function"
+  default     = "life-sync-function"
+}
+
+variable "functions" {
+  type = list(object({
+    name = string
+    entry_point = string
+    source_dir = string
+  }))
+  default = [
+    {
+      name = "life-sync-function"
+      entry_point = "lifeSync"
+      source_dir = "life-sync-function"
+    }
+  ]
+}
